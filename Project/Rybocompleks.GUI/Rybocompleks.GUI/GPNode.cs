7256e469616c2a827bf31fcf6ff05b938590d0df
Pavlovich Andrei <mira69.27@gmail.com>
2016-12-03 09:24:26 +0300

    GUI Update

--- Project/Rybocompleks.GUI/Rybocompleks.GUI/GPNode.cs
+++ Project/Rybocompleks.GUI/Rybocompleks.GUI/GPNode.cs
@@ -9,9 +9,68 @@ namespace Rybocompleks.GUI
     [Serializable]
     public class GPNode
     {
-        public string stageName { get; set; }
-        public int houres { get; set; }
-        public int minutes { get; set; }
+        private string stageName;
+        public string StageName
+        {
+            get
+            {
+                return stageName;
+            }
+            set
+            {
+                if(value == null)
+                {
+                    stageName = "";
+                }
+                else
+                {
+                    stageName = value;
+                }
+            }
+        }
+
+        private int houres;
+        public int Houres
+        {
+            get
+            {
+                return houres;
+            }
+
+            set
+            {
+                if (value >= 0)
+                {
+                    houres = value;
+                }
+                else
+                {
+                    houres = 0;
+                }
+            }
+        }
+
+        private int minutes;
+        public int Minutes
+        {
+            get
+            {
+                return minutes;
+            }
+
+            set
+            {
+                if ((value >= 0) && (value < 60))
+                {
+                    minutes = value;
+                }
+                else
+                {
+                    minutes = 0;
+                }
+            }
+        }
+        
         private int temperature=25;
         public int Temperature { 
             get
@@ -31,20 +90,89 @@ namespace Rybocompleks.GUI
                 }
             }
         }
-        public int oxygen { get; set; }
-        public int LightPerDay { get; set; }
-        public double pH { get; set; }
+
+        private int oxygen;
+        public int Oxygen{ 
+            get
+            {
+                return oxygen;
+            }
+            
+            set
+            {
+                if (value >= 0)
+                {
+                    oxygen = value;
+                }
+                else
+                {
+                    oxygen = 0;
+                }
+            }
+        }
+        
+        private int lightPerDay;
+        public int LightPerDay
+        {
+            get
+            {
+                return lightPerDay;
+            }
+            set
+            {
+                if ((value >= 0) && (value <=24 ))
+                {
+                    lightPerDay = value;
+                }
+                else
+                {
+                    lightPerDay = 0;
+                }
+            }
+        }
+
+        public double ph;
+        public double PH
+        {
+            get
+            {
+                return ph;
+            }
+
+            set
+            {
+                if ((value >= 0) && (value <=20))
+                {
+                    ph = value;
+                }
+                else
+                {
+                    ph = 7;
+                }
+            }
+        }
+        
         public GPNode(){}
         public GPNode(string stageName = "имя интсрукции", int houres = 0, int minutes = 0, 
             int temperature = 25, int oxygen = 20, int LightPerDay = 12, int pH = 7)
         {
-           this.stageName = stageName;
-           this.houres = houres;
-           this.minutes = minutes;
-           this.temperature = temperature;
-           this.oxygen = oxygen;
+           this.StageName = stageName;
+           this.Houres = houres;
+           this.Minutes = minutes;
+           this.Temperature = temperature;
+           this.Oxygen = oxygen;
            this.LightPerDay = LightPerDay;
-           this.pH = pH; 
+           this.PH = pH; 
+        }
+        public GPNode(GPNode gpn)
+        {
+            this.StageName = gpn.StageName;
+            this.Houres = gpn.Houres;
+            this.Minutes = gpn.Minutes;
+            this.Temperature = gpn.Temperature;
+            this.Oxygen = gpn.Oxygen;
+            this.LightPerDay = gpn.LightPerDay;
+            this.PH = gpn.PH;
         }
     }
 }
--- Project/Rybocompleks.GUI/Rybocompleks.GUI/MainWindow.xaml.cs
+++ Project/Rybocompleks.GUI/Rybocompleks.GUI/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -17,145 +18,228 @@ using System.Xml.Serialization;
 
 namespace Rybocompleks.GUI
 {
-       public partial class MainWindow : Window    
-       {
-           private ObservableCollection<GPNode> gpList= new ObservableCollection<GPNode>();
-           private string gpFilePath = null;
-           private void LoadGP()
-           {
-               if (!System.IO.File.Exists(gpFilePath))
-               {
-                   MessageBox.Show("указанный файл (" + gpFilePath + ") отсутсвует", "Ошибка!");
-               }
-               XmlSerializer formatter = new XmlSerializer(typeof(ObservableCollection<GPNode>));
-               using (FileStream fs = new FileStream(gpFilePath, FileMode.OpenOrCreate))
-               {
-                   gpList = (ObservableCollection<GPNode>)formatter.Deserialize(fs);
-               }
-           }
-           private bool SaveGP()
-           {
-               XmlSerializer formatter = new XmlSerializer(typeof(ObservableCollection<GPNode>));
-               
-               if (File.Exists(gpFilePath)) File.Delete(gpFilePath);                   
-
-               using (FileStream fs = new FileStream(gpFilePath, FileMode.OpenOrCreate))
-               {
-                   formatter.Serialize(fs, gpList);
-                   return true;
-               }            
-           }
-           private void UpdateWindow()
-           {
-              // if(gpFilePath!= null)              
-               // this.Title = gpFilePath + " - Рыбокомплекс";
-              // else
-              //  this.Title = "Новый палан - Рыбокомплекс";
-               gpDataGrid.ItemsSource = null;
-               gpDataGrid.ItemsSource = gpList;
-           }
-           
-           public MainWindow()
+    public partial class MainWindow : Window
+    {
+        private GPNode clipboard;
+        public GPNode Clipboard
+        {
+            get { return clipboard; }
+            set
             {
-                InitializeComponent();
-                gpDataGrid.ItemsSource = gpList;
-            }
-
-           private void SaveAs_BtnClick(object sender, RoutedEventArgs e)
-               {
-                   Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
-                   dlg.FileName = "План выращивания"; // Default file name
-                   dlg.DefaultExt = ".xml"; // Default file extension
-                   dlg.Filter = "xml documents (.xml)|*.xml"; // Filter files by extension
-                   // Show open file dialog box
-                   Nullable<bool> result = dlg.ShowDialog();
-                   if (result == true)
-                   {
-                       gpFilePath = dlg.FileName;
-                       bool tmp = SaveGP();
-                       if (tmp)
-                       {
-                           MessageBox.Show("Сохранено");
-                       }
-                       else
-                       {
-                           MessageBox.Show("Не сохранено");
-                       }
-                   }
-               }
-
-           private void Save_BtnClick(object sender, RoutedEventArgs e)
-           {
-                if (!File.Exists(gpFilePath))
+                if (clipboard != value)
                 {
-                    SaveAs_BtnClick(sender, e);
+                    clipboard = value;
+                }
+                if (clipboard != null)
+                {
+                    InsertIsEnabled(true);
                 }
                 else
                 {
-                    bool tmp = SaveGP();
-                    if (tmp)
-                    {
-                        MessageBox.Show("Сохранено");
-                    }
-                    else
-                    {
-                        MessageBox.Show("Не сохранено");
-                    }
+                    InsertIsEnabled(false);
                 }
             }
-           
-           private void Open_BtnClick(object sender, RoutedEventArgs e)
-           {
-               Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-               dlg.FileName = ""; // Default file name
-               dlg.DefaultExt = ".xml"; // Default file extension
-               dlg.Filter = "xml documents (.xml)|*.xml"; // Filter files by extension
-               // Show open file dialog box
-               Nullable<bool> result = dlg.ShowDialog();
-               // Process open file dialog box results
-               if (result == true)
-               {
-                   gpFilePath = dlg.FileName;
-                   LoadGP();
-                   UpdateWindow();
-               }     
-           }
+        }
 
-           private void New_BtnClick(object sender, RoutedEventArgs e)
-           {
-               MessageBoxResult dialogResult = MessageBox.Show("Все несохранённые данные будут утеряны! \nПродолжить?", "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Warning);
-               if (dialogResult == MessageBoxResult.Yes)
-               {
-                   gpFilePath = null;
-                   gpList = new ObservableCollection<GPNode>();
-                   UpdateWindow();
-               }               
-           }
+        private void InsertIsEnabled(bool b)
+        {
+            MenuEditInsert.IsEnabled = b;
+            InsertBtn.IsEnabled = b;
+            ContextMenuInsert.IsEnabled = b;
+        }
 
-           private void Exit_Click(object sender, RoutedEventArgs e)
-           {
-               MessageBoxResult dialogResult = MessageBox.Show("Все несохранённые данные будут утеряны! \nВыйти?", "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Warning);
-               if (dialogResult == MessageBoxResult.Yes)
-                   this.Close();                
-           }
 
-           private void del_BtnClick(object sender, RoutedEventArgs e)
-           {
-               int i = gpDataGrid.SelectedIndex;
-               try
-               {
-                   gpList.Remove(gpList[i]);
-               }
-               catch (ArgumentOutOfRangeException)
-               { }
-           }
+        private void EditCopyClick(object sender, RoutedEventArgs e)
+        {
+            if (gpDataGrid.SelectedIndex >= 0 && gpDataGrid.SelectedIndex < gpList.Count && gpList != null)
+            {
+                this.Clipboard = new GPNode(gpList[gpDataGrid.SelectedIndex]);
+            }
+        }
 
-           private void RunGC_BtnClick(object sender, RoutedEventArgs e)
-           {
-               GrowingCycleWindow gcw = new GrowingCycleWindow();
-               gcw.Show();
-           }
-        
-      }
+        private void EditInsertClick(object sender, RoutedEventArgs e)
+        {
+            if (Clipboard != null && gpList != null)
+            {
+                if (gpDataGrid.SelectedIndex >= 0 && gpDataGrid.SelectedIndex < gpList.Count + 1)
+                {
+                    gpList.Insert(gpDataGrid.SelectedIndex, new GPNode(Clipboard));
+                    UpdateWindow();
+                }
+                else
+                {
+                    gpList.Insert(gpList.Count, new GPNode(Clipboard));
+                }
+            }
+        }
 
+        private void EditDeleteClick(object sender, RoutedEventArgs e)
+        {
+            int i = gpDataGrid.SelectedIndex;
+            try
+            {
+                gpList.Remove(gpList[i]);
+            }
+            catch (ArgumentOutOfRangeException)
+            { }
+        }
+
+
+        private void EditCutClick(object sender, RoutedEventArgs e)
+        {
+            EditCopyClick(sender, e);
+            EditDeleteClick(sender, e);
+        }
+
+
+        private ObservableCollection<GPNode> gpList;
+
+        private string gpFilePath = null;
+        private void LoadGP()
+        {
+            if (!System.IO.File.Exists(gpFilePath))
+            {
+                MessageBox.Show("указанный файл (" + gpFilePath + ") отсутсвует", "Ошибка!");
+            }
+            XmlSerializer formatter = new XmlSerializer(typeof(ObservableCollection<GPNode>));
+            using (FileStream fs = new FileStream(gpFilePath, FileMode.OpenOrCreate))
+            {
+                gpList = (ObservableCollection<GPNode>)formatter.Deserialize(fs);
+            }
+        }
+        private bool SaveGP()
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(ObservableCollection<GPNode>));
+
+            if (File.Exists(gpFilePath)) File.Delete(gpFilePath);
+
+            using (FileStream fs = new FileStream(gpFilePath, FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, gpList);
+                return true;
+            }
+        }
+
+        public MainWindow()
+        {
+            gpList = new ObservableCollection<GPNode>();
+            InitializeComponent();
+            gpDataGrid.ItemsSource = gpList;
+            Clipboard = null;
+        }
+
+        private void SaveAs_BtnClick(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "План выращивания"; // Default file name
+            dlg.DefaultExt = ".xml"; // Default file extension
+            dlg.Filter = "xml documents (.xml)|*.xml"; // Filter files by extension
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                gpFilePath = dlg.FileName;
+                bool tmp = SaveGP();
+                if (tmp)
+                {
+                    MessageBox.Show("Сохранено", "", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Не сохранено", "", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void Save_BtnClick(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists(gpFilePath))
+            {
+                SaveAs_BtnClick(sender, e);
+            }
+            else
+            {
+                bool tmp = SaveGP();
+                if (tmp)
+                {
+                    MessageBox.Show("Сохранено");
+                }
+                else
+                {
+                    MessageBox.Show("Не сохранено");
+                }
+            }
+        }
+
+        private void Open_BtnClick(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = ""; // Default file name
+            dlg.DefaultExt = ".xml"; // Default file extension
+            dlg.Filter = "xml documents (.xml)|*.xml"; // Filter files by extension
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+            // Process open file dialog box results
+            if (result == true)
+            {
+                gpFilePath = dlg.FileName;
+                LoadGP();
+                UpdateWindow();
+            }
+        }
+
+        private void New_BtnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult dialogResult = MessageBox.Show("Все несохранённые данные будут утеряны! \nПродолжить?", "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (dialogResult == MessageBoxResult.Yes)
+            {
+                gpFilePath = null;
+                gpList = new ObservableCollection<GPNode>();
+                UpdateWindow();
+                Save_BtnClick(sender, e);
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult dialogResult = MessageBox.Show("Все несохранённые данные будут утеряны! \nВыйти?", "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (dialogResult == MessageBoxResult.Yes)
+                this.Close();
+        }
+
+
+        private void RunGC_BtnClick(object sender, RoutedEventArgs e)
+        {
+            GrowingCycleWindow gcw = new GrowingCycleWindow();
+            gcw.Show();
+        }
+
+        private void UpdateWindow()
+        {
+            // if(gpFilePath!= null)              
+            // this.Title = gpFilePath + " - Рыбокомплекс";
+            // else
+            //  this.Title = "Новый палан - Рыбокомплекс";
+            gpDataGrid.ItemsSource = null;
+            gpDataGrid.ItemsSource = gpList;
+        }
+
+        private void SelectLineClick(object sender, RoutedEventArgs e)
+        {
+            if (gpList.Count > 1)
+            {
+                SelectStringWindow ssw = new SelectStringWindow(gpList.Count);
+                if (ssw.ShowDialog() == true)
+                {
+                    try
+                    {
+                        gpDataGrid.SelectedIndex = ssw.LineNumber - 1;
+                    }
+                    catch (ArgumentOutOfRangeException)
+                    { }
+                }
+            }           
+        }
+
+    }
 }
