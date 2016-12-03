266dae094b582cd1b8b943840b2ddc9877916b9a
Maxim Lukoshko <www.maxim.by97@gmail.com>
2016-12-03 18:22:02 +0300

    Добавлено устройство "Регулятор кислорода"

---
+++ Project/Rybocompleks.GUI/Rybocompleks.Perepherial/Devices/OxygenDevice.cs
@@ -0,0 +1,39 @@
+﻿using Perepherial.Classes;
+using Rybocompleks.Data;
+using Rybocompleks.Data.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perepherial.Devices
+{
+    class OxygenDevice : Device
+    {
+        public OxygenDevice(Location loc) : base(loc)
+        {
+            SetState( new OxygenMeasurment(0) );
+        }
+        public override void SetState(Rybocompleks.Data.IMeasurment state)
+        {
+            State = null != (IOxygenMeasurrment)state ? state : new OxygenMeasurment(0);
+            Nature.Oxygen = (IOxygenMeasurrment)State;
+        }
+
+        public override int GetPropertyID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetName()
+        {
+            return "Регулятор кислорода";
+        }
+
+        public override int GetIcon()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
