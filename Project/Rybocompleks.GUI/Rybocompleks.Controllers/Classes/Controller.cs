3c2f40f691397647d01a0b6f6f1383721865f336
Maxim Lukoshko <www.maxim.by97@gmail.com>
2016-12-03 21:26:32 +0300

    Добавлен абстрактный контроллер

---
+++ Project/Rybocompleks.GUI/Rybocompleks.Controllers/Classes/Controller.cs
@@ -0,0 +1,35 @@
+﻿using Rybocompleks.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rybocompleks.Controllers.Classes
+{
+    
+    class Controller<IPhysObjType> where IPhysObjType : IPhysicalObject
+    {
+        protected IDictionary<Int32, IPhysObjType> devices;
+
+        public Controller()
+        {
+            devices = new Dictionary<Int32, IPhysObjType>();
+        }
+
+        public ICollection<IShowInfo> GetShowInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddObject(IPhysObjType obj)
+        {
+            devices.Add(obj.GetPropertyID(), obj);
+        }
+
+        public IDictionary<Int32, IPhysObjType> GetPhysicalObjects()
+        {
+            return devices;
+        }
+    }
+}
