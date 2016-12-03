19779a31302178ea9c578cb86a117ee3e704f224
Maxim Lukoshko <www.maxim.by97@gmail.com>
2016-12-03 18:18:11 +0300

    Добавлено устройство "Нагреватель"

---
+++ Project/Rybocompleks.GUI/Rybocompleks.Perepherial/Devices/TemperatureDevice.cs
@@ -0,0 +1,39 @@
+﻿using Perepherial.Classes;
+using Rybocompleks.Data;
+using Rybocompleks.Data.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perepherial.Devices
+{
+    class TemperatureDevice : Device
+    {
+        public TemperatureDevice(Location loc) : base(loc)
+        {
+            SetState( new TemperatureMeasurment(0) );
+        }
+        public override void SetState(Rybocompleks.Data.IMeasurment state)
+        {
+            State = null != (ITemperatureMeasurment)state ? state : new TemperatureMeasurment(0);
+            Nature.Temperature = (ITemperatureMeasurment)State;
+        }
+
+        public override int GetPropertyID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetName()
+        {
+            return "Нагреватель";
+        }
+
+        public override int GetIcon()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
