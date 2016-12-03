0a360fec011ad640875aa4138574eed079dd6aea
Maxim Lukoshko <www.maxim.by97@gmail.com>
2016-12-03 18:29:41 +0300

    Добавлены устройства PHDevice и LightDevice

---
+++ Project/Rybocompleks.GUI/Rybocompleks.Perepherial/Devices/LightDevice.cs
@@ -0,0 +1,38 @@
+﻿using Perepherial.Classes;
+using Rybocompleks.Data.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perepherial.Devices
+{
+    class LightDevice : Device
+    {
+        public LightDevice(Location loc) : base(loc)
+        {
+            SetState(new LightMeasurment(false));
+        }
+        public override void SetState(Rybocompleks.Data.IMeasurment state)
+        {
+            State = null != (LightMeasurment)state ? state : new LightMeasurment(false);
+            Nature.Light = (LightMeasurment)State;
+        }
+
+        public override int GetPropertyID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetName()
+        {
+            return "Лампа";
+        }
+
+        public override int GetIcon()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Project/Rybocompleks.GUI/Rybocompleks.Perepherial/Devices/PhDevice.cs
@@ -0,0 +1,38 @@
+﻿using Perepherial.Classes;
+using Rybocompleks.Data.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perepherial.Devices
+{
+    class PhDevice : Device
+    {
+        public PhDevice (Location loc) : base(loc)
+        {
+            SetState( new PHMeasurment(0) );
+        }
+        public override void SetState(Rybocompleks.Data.IMeasurment state)
+        {
+            State = null != (PHMeasurment)state ? state : new PHMeasurment(0);
+            Nature.PH = (PHMeasurment)State;
+        }
+
+        public override int GetPropertyID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetName()
+        {
+            return "Регулятор кислотности";
+        }
+
+        public override int GetIcon()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
