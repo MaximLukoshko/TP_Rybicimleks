1406fa9666ac3c0310ab4db0aca98066424f4552
Maxim Lukoshko <www.maxim.by97@gmail.com>
2016-12-13 14:02:45 +0300

    GPCommon перенесён в Dispatcher GrowingPlan и IGrowingPlan перенесены в GUI Удалён компонент GrowingPlan

---
+++ Project/Rybocompleks.GUI/Rybocompleks.Dispatcher/Classes/GrowingPlanCommon.cs
@@ -0,0 +1,36 @@
+﻿using Rybocompleks.Data;
+using Rybocompleks.GrowingPlan;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rybocompleks.GrowingPlan
+{
+    public class GrowingPlanCommon : IGrowingPlanCommon
+    {
+        protected IList<GPAllowedStates> AllowedStatesList;
+        public GrowingPlanCommon(ICollection<IToGPAllowedStates> allowedStatesList)
+        {
+            AllowedStatesList = new List<GPAllowedStates>();
+            foreach(IToGPAllowedStates state in allowedStatesList)
+            {
+                AllowedStatesList.Add(state.ToGPAllowedStates());
+            }
+        }
+        public IDictionary<MeasurmentTypes.Type, IInstruction> GetAllowedStates(Int32 hours, Int32 minutes)
+        {
+            DateTime required = (new DateTime()).AddHours(hours).AddMinutes(minutes);
+            for (Int32 i = 0; i < AllowedStatesList.Count - 1; i++)
+            {
+                DateTime currentInstructionTime = (new DateTime()).AddHours(AllowedStatesList[i].Hours).AddMinutes(AllowedStatesList[i].Minutes);
+                DateTime nextInstructionTime = (new DateTime()).AddHours(AllowedStatesList[i + 1].Hours).AddMinutes(AllowedStatesList[i + 1].Minutes);
+
+                if (currentInstructionTime <= required && nextInstructionTime > required)
+                    return AllowedStatesList[i].AllowedStates;
+            }
+            return null;
+        }
+    }
+}
